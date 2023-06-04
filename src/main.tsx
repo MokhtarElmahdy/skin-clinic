dd777c09d5739869e5cb4dee7046188b338aed77
MokhtarElmahdy <mekhaelmahdy@gmail.com>
2023-06-04 06:13:26 +0300

    commenting unused variables

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import './index.css'
-import { store } from './redux/store/store.js';
-import { Provider } from 'react-redux';
+// import { store } from './redux/store/store.js';
+// import { Provider } from 'react-redux';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
